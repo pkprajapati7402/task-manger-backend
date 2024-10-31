fdd9eb583460b19927a68b416ff3c885654ca588
Prince <167101466+pkprajapati7402@users.noreply.github.com>
2024-10-30 23:16:10 -0700

    Update authMiddleware.js

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken');
 
 module.exports = (req, res, next) => {
-  const token = req.header('Authorization');
+  const token = req.header('Authorization')?.split(' ')[1]; // Get token after "Bearer"
   if (!token) return res.status(401).json({ message: 'No token, authorization denied' });
 
   try {
